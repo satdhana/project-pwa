78c3c7223be5563061b81fd8003977d754760eaa
DESKTOP-BBARBC1\IT NBA <dafaputra35@gmail.com>
2022-10-26 14:05:27 +0700

    migrasi komponen template BWA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./tailwind.css";
+import Header from "./component/Header";
+import Hero from "./component/Hero";
+import Browse from "./component/Browse";
+import Arrived from "./component/Arrived";
+import Client from "./component/Client";
+import Aside from "./component/AsideMenu";
+import Footer from "./component/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Client />
+      <Aside />
+      <Footer />
+    </>
   );
 }
 
